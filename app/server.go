ed151528db79e60ee426582ba9e85e93502b2d73
rtanwar <tanwar.r@gmail.com>
2017-11-16 11:48:04 +0000

    separate login index

--- app/server.go
+++ app/server.go
@@ -1,16 +1,25 @@
 package main
 
-import ("gopkg.in/macaron.v1"
+import ("gopkg.in/macaron.v1";
+"fmt"
 )
 
 func main() {
 	m := macaron.Classic()
-m.Use(macaron.Renderer(macaron.RenderOptions{
-			Directory:  "templates",              
-			Extensions: []string{".tmpl", ".html"}, 
-}))
+        m.Use(macaron.Renderer())
+//m.Use(macaron.Renderer(macaron.RenderOptions{
+//			Directory:  "templates",              
+//			Extensions: []string{".tmpl", ".html"}, 
+//}))
 	m.Get("/", func(ctx *macaron.Context) {
+                fmt.Printf("%s\n", "/")
+                ctx.Data["title"] = "Index Page"
 		ctx.HTML(200, "pages/index")
 	})
+        m.Get("/login", func(ctx *macaron.Context) {
+                fmt.Printf("%s\n", "/login") 
+                ctx.Data["title"] = "Login Page"
+                ctx.HTML(200, "pages/login")
+        })
 	m.Run()
 }
